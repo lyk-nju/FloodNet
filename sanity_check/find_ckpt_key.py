51e4841e95a832a6028ac0ced4a23d2f9980130f
lyk-nju <981125269@qq.com>
2026-05-14 13:58:02 +0800

    fix stream metric bug

--- sanity_check/find_ckpt_key.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-ckpt = torch.load("/data/home/shengqiuProf_user_yuankai/FloodNet/outputs/20260508_001434_ldf/step_392000.ckpt", map_location='cpu', weights_only=False)
-
-print('has ema_state:', 'ema_state' in ckpt)
-print('global_step:', ckpt.get('global_step'))
-
-if 'ema_state' in ckpt:
-    print('shadow_params count:', len(ckpt['ema_state']['shadow_params']))
