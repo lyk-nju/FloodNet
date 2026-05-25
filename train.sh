529528f72a453f2209aaf17421bb891bba0da737
lyk-nju <981125629@qq.com>
2026-05-25 19:50:37 +0800

    优化结构

--- train.sh
+++
@@ -1,9 +0,0 @@
-# use "setproxy" in tmux first
-# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True,max_split_size_mb:128
-# export HF_ENDPOINT="https://hf-mirror.com"
-
-source /opt/anaconda3/bin/activate
-
-conda activate flooddiffusion
-
-python train_ldf.py --config configs/ldf_babel.yaml
